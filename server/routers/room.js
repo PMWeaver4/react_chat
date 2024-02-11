9190479e344b438df8ca80dca98e320808ff1f1f
ttnclinton <ttnclinton@gmail.com>
2024-02-10 20:04:14 -0700

    need to add coding in room.js for addUsers

---
+++ server/routers/room.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const room = require("./roon");
+
+const RoomSchema = new mongoose.Schema({
+    text: {
+        name: {type: String,
+            required: true,
+            minlength: 1},
+        descrition: {type: String,
+            required: true,
+            minlength: 1},
+        addUsers: ,
+        
+    }
+    
+});
+
+module.exports = mongoose.model("room", RoomSchema);
