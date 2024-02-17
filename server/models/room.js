661f75142f41361a8c3ab4074473b62cd0b1c9b9
ttnclinton <ttnclinton@gmail.com>
2024-02-17 12:16:01 -0700

    Added display all rooms endpoint to room.routes. Look for 2/17 notes

--- server/models/room.js
+++ server/models/room.js
@@ -3,22 +3,34 @@ const room = require("./room");
 
 const RoomSchema = new mongoose.Schema({
     
-        name: {type: String,
+        name: {
+            type: String,
             required: true,
-            minlength: 1},
-        description: {type: String,
-            required: true,
-            minlength: 1},
-// HEAD:server/routers/room.js
-        addUsers: {},
-//
-        addUsers: {
-            type: Array,
+
+            //? Commented out line 11 on 2/17. Do we need minlength for name?
+            // minlength: 1,
         },
+        description: {
+            type: String,
+            required: true,
+            minlength: 1,
+        },
+
+ //? Redid user coding 2/17. Let me know if this works or not.       
+        addedUsers: {
+            type: [String],
+            required: true,
+        }
+// I commented out line 25-31 so line 20 can be tested on your end. 2/17 TC
+
+// HEAD:server/routers/room.js
+//         addUsers: {},        
+// 
+//        addUsers: {
+//            type: Array,
+ //       },
 // 59c297e8c7ff53ab6051c03f1eafd42a5c263b26:server/models/room.js
-        
-    
-    
+           
 },
 {
     //test this out when doing routes
