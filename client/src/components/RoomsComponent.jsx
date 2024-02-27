ddb563a929380088f3444654d42df570e788c4fd
PMWeaver4 <paulmarrottweaver@gmail.com>
2024-02-27 15:01:23 -0500

    rooms component added

---
+++ client/src/components/RoomsComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const RoomsComponent = () => {
+  return (
+    <div>RoomsComponent</div>
+  )
+}
+
+
--- client/src/components/rooms_component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const rooms_component = () => {
-  return (
-    <div>rooms_component</div>
-  )
-}
-
-export default rooms_component
