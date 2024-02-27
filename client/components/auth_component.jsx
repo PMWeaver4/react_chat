a54f46e79d640b7100d049e4b74c6bc413850a0a
PMWeaver4 <paulmarrottweaver@gmail.com>
2024-02-26 19:57:19 -0500

    signup maybe works

--- client/components/auth_component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const auth_component = () => {
-  return (
-    <div>auth_component</div>
-  )
-}
-
-export default auth_component
---
+++ client/src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+// import {Routes, Route, Navigate} from "react-router-dom";
+// import {AuthComponent, rooms_component} from "./components";
+import {AuthComponent} from "./components/AuthComponent";
+import './App.css'
+
+function App() {
+  
+  const [sessionToken, setSessionToken] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (state, value) => {
+    switch(state){
+      case "first":
+        setFirstName(value);
+        break;
+      case "last":
+        setLastName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+        default:
+          console.log("something went wrong");
+    }
+}
+
+const handleSignup = async() => {
+try{
+
+  const response = await(await fetch("http://localhost:7000/user/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    }),
+  })
+).json()
+console.log(response);
+}catch(err){
+  console.log(err)
+}
+}
+
+  return (
+    <>
+{/*     
+    {firstName}
+    {lastName}
+    {email}
+    {password}
+    {!sessionToken ?
+
+      <Signup handleChange={handleChange} handleSignup={handleSignup}/>
+      :
+      <>
+      <button>Logout</button>
+      <p>[Products]</p>
+      </>
+    }
+     */}
+     something, anything
+     <AuthComponent handleChange={handleChange} handleSignup={handleSignup} />
+    </>
+  )
+}
+
+export default App
