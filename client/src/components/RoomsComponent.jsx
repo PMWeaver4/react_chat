90cbcb2f4dbfe115e4f515c9373511b3ea1b2307
PMWeaver4 <paulmarrottweaver@gmail.com>
2024-02-28 20:31:04 -0500

    retrying

--- client/src/components/RoomsComponent.jsx
+++ client/src/components/RoomsComponent.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export const RoomsComponent = () => {
 
+const [displayRooms, setDisplayRooms] = useState("");
+
+  const showRooms = async() => {
+    try {
+      setDisplayRooms("zippity doo dah")
+    } catch (err){
+      console.log(err);
+    }
+  }
   
   return (
+    {displayRooms}
     <div>RoomsComponent</div>
     
   )
