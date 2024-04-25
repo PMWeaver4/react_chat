82474b26159bc0e2c92ee09b679a6e2c497b242a
PMWeaver4 <paulmarrottweaver@gmail.com>
2024-04-25 10:51:05 -0400

    added update user

--- server/models/user.js
+++ server/models/user.js
@@ -1,38 +1,33 @@
 const mongoose = require("mongoose");
 const user = require("./user");
 
-const UserSchema = new mongoose.Schema ({
-    
-        firstName: {
-            type: String,
-            required: true,
-            minlength: 1,
-        },
-        lastName: {
-            type: String,
-            required: true,
-            minlength: 1,
-        },
-        email: {
-            type: String,
-            required: true,
-            minlength: 1,
-            unique: true,
-        },
-        password: {
-            type: String,
-            unique: true,
-            minlength: 1,
-        }
-
-        
-        
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 1,
     },
-    {
-    timestamps: true
-    }
-
-
+    lastName: {
+      type: String,
+      required: true,
+      minlength: 1,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 1,
+      unique: true,
+    },
+    password: {
+      type: String,
+      unique: true,
+      minlength: 1,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("user", UserSchema);
